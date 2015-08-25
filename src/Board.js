9ff02890819361681993c4f654a9b1d0b39cde9a
brettwsanders <bsand08@gmail.com>
2015-08-24 20:15:53 -0700

    Work

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,24 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var rows = this.rows();
+
+      return rows[rowIndex].reduce(function(total, item) {
+        return total + item;
+      }, 0) > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // console.log(this);
+      var rows = this.rows();
+      var result = false;
+      rows.forEach(function(rowsArray, index) {
+        if(this.hasRowConflictAt(index) === true) {
+          result = true;
+        }
+      }.bind(this));
+      return result;
     },
 
 
@@ -94,12 +106,21 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var rows = this.rows();
+      return rows.reduce(function(total, item) {
+        return total + item[colIndex];
+      }, 0) > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var columns = this.rows();
+      for (var i = 0; i < columns.length; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,12 +130,27 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var result = false;
+      var rows = this.rows();
+      var counter = 0;
+      for (var i = 0; i < rows.length; i++) {
+        counter = counter + ((rows[i][i + majorDiagonalColumnIndexAtFirstRow]) || 0);
+      };
+      return counter > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var startingFirstRowIndex = this.rows().length * (-1) + 1;
+      var rows = this.rows();
+      for (var i = startingFirstRowIndex; i < rows.length; i++) {
+        if(this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      };
+
+      return false;
+
     },
 
 
@@ -124,12 +160,26 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var result = false;
+      var rows = this.rows();
+      var counter = 0;
+      for (var i = 0; i < rows.length ; i++) {
+        counter = counter + ((rows[i][minorDiagonalColumnIndexAtFirstRow - i]) || 0);
+      };
+      return counter > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+      var startingFirstRowIndex = (rows.length - 1) * (2) - 1;
+      for (var i = startingFirstRowIndex; i > 0; i--) {
+        if(this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      };
+
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
--- src/solvers.js
+++ src/solvers.js
@@ -15,8 +15,86 @@
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
+  var rooksGraph = new Graph();
 
+  for (var row = 0; row < n; row++) {
+    for (var column = 0; column < n; column++) {
+      var positionNode = new Node([row, column]);
+      rooksGraph.nodes.push(positionNode);
+    };
+  };
+
+  rooksGraph.nodes.forEach(function(node){
+    var nodeColumn = node.position[1];
+    var nodeRow = node.position[0];
+    // debugger;
+    for (var nodeIndex = 0; nodeIndex < rooksGraph.nodes.length; nodeIndex++) {
+      var currentComparison = rooksGraph.nodes[nodeIndex];
+      // debugger;
+      if ((currentComparison.position[0] === nodeRow || currentComparison.position[1] === nodeColumn) && !(nodeRow !== currentComparison.position[0] && nodeColumn !== currentComparison.position[1])) {
+        node.edges.push(currentComparison);
+      } else if (nodeRow === currentComparison.position[0] && nodeColumn === currentComparison.position[1]) {
+        /* Do nothing! */
+      } else {
+        node.safeEdges.push(currentComparison);
+      }
+    };
+  });
+
+  var allAnswers = []
+  /* Iteration Traversal */
+  for (var i = 0; i < rooksGraph.nodes.length; i++) {
+
+    var allAnswersFromThisstartingNode = [];
+
+    var recursiveTraversal = function(node, placedPositions, invalidEdges) {
+      placedPositions = placedPositions || [];
+      invalidEdges = invalidEdges || [];
+      invalidEdges.concat(node.edges);
+
+      /* Base Case */
+      if (placedPositions.length === n) {
+        allAnswersFromThisstartingNode.push(placedPositions);
+        return;
+      }
+
+      /* If no where to go */
+      
+
+      node.safeEdges.forEach(function(safeEdge) {
+        if(placedPositions.length === 0 ){
+          placedPositions.push(safeEdge);
+          /* Recurse */
+          recursiveTraversal(safeEdge, placedPositions, invalidEdges);
+        } else {
+          var isSafe = true;
+          for (var i = 0; i < safeEdge.edges.length; i++) {
+            for (var i = 0; i < placedPositions.length; i++) {
+              if( (placedPositions[i].position[0] === safeEdge.edges[i].position[0]) && (placedPositions[i].position[1] === safeEdge.edges[i].position[1]) ) {
+                isSafe = false;
+              }
+            };
+          };
+          if (isSafe) {
+            placedPositions.push(safeEdge);
+            /* Recurse */
+            recursiveTraversal(safeEdge, placedPositions, invalidEdges);
+          }
+        }
+      });
+    }
+
+
+    
+    recursiveTraversal(rooksGraph.nodes[i]);
+    allAnswers.push(allAnswersFromThisstartingNode);
+  };
+
+  /* Build a board from all answers */
+
+  console.log(allAnswers);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  console.log(rooksGraph);
   return solution;
 };
 
@@ -48,3 +126,40 @@ window.countNQueensSolutions = function(n) {
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+var Node = function(position) {
+  this.position = position;
+  this.edges = [];
+  this.safeEdges = [];
+
+  this.setEdges = function(passedEdges) {
+    var indexToSplice;
+    var that = this;
+    var copyOfPassedEdges = passedEdges.slice();
+    copyOfPassedEdges.forEach(function(copiedEdge, indexOfCopy) {
+        if (JSON.stringify(copiedEdge.position) === JSON.stringify(that.position)) {
+          indexToSplice = indexOfCopy;
+        }
+    });
+    copyOfPassedEdges.splice(indexToSplice, 1);
+    this.edges.concat(copyOfPassedEdges);
+  };
+};
+
+var Graph = function() {
+  this.nodes = [];
+  this.size = 0;
+  this.add = function(position) {
+    var node = new Node(position);
+    this.nodes.push(node);
+    this.size++;
+  };
+};
+
+function testingFunction() {
+  findNRooksSolution(3);
+}
+
+testingFunction();
+
+
